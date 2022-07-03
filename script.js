a6db8c77f5a73e471d1986a84ee446b5ffc6460c
jyddys1 <justus.sydanmaa@hotmail.com>
2022-07-03 19:28:26 +0300

    RockPaperScissors got computerPlay function

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+function computerPlay() { //computer generates a random answer.
+    let random = ["rock", "paper", "scissors"];
+    console.log(random[Math.floor(Math.random() * 3)]);
+  
+}
+
+computerPlay()
